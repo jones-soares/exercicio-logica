c8c82c083824618e6dafda432cd8ddf67324192d
jones-soares <jonesapsoares@gmail.com>
2024-11-11 13:57:01 -0300

    Desafio 01 - Logica Programação - DIO

---
+++ jogo_01.js
@@ -0,0 +1,27 @@
+
+// Pede o nome do herói e a quantida xp
+const nomeHeroi = "Jones";
+const xp=10001
+
+// Define os níveis de acordo com o XP
+let nivel;
+if (xp < 1000) {
+  nivel = "Ferro";
+} else if (xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp <= 5000) {
+  nivel = "Prata";
+} else if (xp <= 7000) {
+  nivel = "Ouro";
+} else if (xp <= 8000) {
+  nivel = "Platina";
+} else if (xp <= 9000) {
+  nivel = "Ascendente";
+} else if (xp <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+
+// Exibe o resultado
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
